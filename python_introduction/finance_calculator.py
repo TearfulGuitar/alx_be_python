3029f545126b3171eb09ac2ca9f1498667597793
TearfulGuitar <robert254joe@gmail.com>
2024-09-13 12:29:01 +0300

    add task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+monthly_income = int(input ("Enter your monthly income"))
+total_monthly_expenses = int(input ("Enter your total monthly expenses"))
+monthly_savings = total_monthly_expenses - monthly_income
+Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print = ("Projected_Savings")
