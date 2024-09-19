515a04f8a8e6337e4329540196fdd8794f5ff9e8
TearfulGuitar <robert254joe@gmail.com>
2024-09-19 15:59:10 +0300

    Add changes

--- python_introduction/basic_operations.py
+++
@@ -1,6 +0,0 @@
-number1 = 10
-number2 = 5
-operation = number1 + number2
-operation = number1 - number2
-operation = number1 * number2
-print ("[operation] of [number1] and [number2] is [result]")
--- python_introduction/control-flow/weather_advice.py
+++
@@ -1,9 +0,0 @@
-weather = input ("What's the weather like today? (sunny/rainy/cold): ")
-if "sunny" :
-    print= ("Wear a t-shirt and sunglasses.")
-elif "rainy" :
-    print= ("Don't forget your umbrella and a raincoat.")
-elif "cold" :
-    print= ("Make sure to wear a warm coat and a scarf.")
-else :
-    print= ("Sorry, I don't have recommendations for this weather.")
