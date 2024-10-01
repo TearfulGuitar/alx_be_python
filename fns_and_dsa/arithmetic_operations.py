58319862bcfb9904fab3a13e96196dcb9ba8fa26
TearfulGuitar <robert254joe@gmail.com>
2024-10-01 23:08:26 +0300

    Python functions

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation (num1,num2,operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero."
+    else:
+        return "Error: Invalid operation."
+    
+num1 = 5
+num2 = 6
+
+result = num1 + num2
+print (f"Result:{result}") 
