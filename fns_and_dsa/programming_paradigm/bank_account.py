f61459fa84c78a6c4d039cd637b0f29045d36819
TearfulGuitar <robert254joe@gmail.com>
2024-10-10 11:20:59 +0300

    configured OOP

---
+++ fns_and_dsa/programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount :
+  def __init__(self,account_balance) :
+    return 'Initial balance is zero'
+
+def deposit(amount) :
+  return '+= account_balance'
+
+def withdraw(amount) :
+  return '-= account_balance'
+  if account_balance >= withdraw(amount):
+    return 'True'
+  else:
+    return 'False'
+
+def display_balance():
+  print (f"(amount) is your current balance")
