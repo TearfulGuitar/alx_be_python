09530633b4348e2f663a85f5de62d72a22749a31
TearfulGuitar <robert254joe@gmail.com>
2024-10-10 12:42:51 +0300

    Configured OOP

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float (numerator)
+        denom = float (denominator)
+        result = num/denom 
+
+        return f"Result: {result}"
+    
+    except ZeroDivisionError:
+        return "Error! Division by zero is not allowed"
+
+    except ValueError:
+        return "Error! Non numeric input.Please enter valid numbers"
+    
