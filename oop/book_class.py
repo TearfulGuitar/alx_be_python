1c3c1dd53d8d014c3f1b40f720eafa64781a75da
TearfulGuitar <robert254joe@gmail.com>
2024-10-12 19:31:29 +0300

    configured code in oop

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,9 +2,7 @@ class Book:
     def __init__(self,title,author,year) :
         self.title= title
         self.author= author
-        self.year= year
-    
-    
+        self.year= year  
     def _str_(self):
         return "f{self.title} by {self.author}, published in {self.year}"
     
