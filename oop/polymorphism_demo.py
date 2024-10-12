9ff91a237e1b5ac4165c4acee3e73eec6b4b3553
TearfulGuitar <robert254joe@gmail.com>
2024-10-12 21:25:33 +0300

    configured code in oop

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -2,24 +2,25 @@ import math
 
 
 class Shape:
-    def __init__(self,area) :
-        pass 
+    def __init__(self) -> None:
+        pass
 
     def area(self):
         pass
 
-class Rectangle (Shape):
-    def __init__(self, length,width):
-       self.length = length
-       self.width = width
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
     def area(self):
-       return {self.length}*{self.width}
+        return self.length * self.width
 
-class Circle (Shape):
+
+class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
-    
+
     def area(self):
-        return math.pi*{self.radius**2}
-    
+        return math.pi * (self.radius ** 2)
